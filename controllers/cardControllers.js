8dff4d5d08ac9dda07aaa77e9799ce522c048650
OleksandrLukomskyi <LUKOMSKYI.OLEKSANDR@GMAIL.COM>
2024-06-08 22:16:35 +0200

    update branch

--- controllers/cardControllers.js
+++ controllers/cardControllers.js
@@ -1,9 +1,43 @@
 import Cards from "../models/cardSchema.js";
 
 export const addCard = async (req, res, next) => {
+  const card = {
+    title: req.body.title,
+    description: req.body.description,
+    priority: req.body.priority,
+    deadline: req.body.deadline,
+  };
   try {
-    const { _id } = req.params;
-    res.status(201).send(_id);
+    const result = await Cards.create(card);
+
+    res.status(201).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateCard = async (req, res, next) => {
+  const card = {
+    title: req.body.title,
+    description: req.body.description,
+    priority: req.body.priority,
+    deadline: req.body.deadline,
+  };
+  try {
+    const result = await Cards.findByIdAndUpdate(_id, card, { new: true });
+
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteCard = async (req, res, next) => {
+  const { _id } = req.params;
+
+  try {
+    const result = Cards.findByIdAndDelete(_id);
+    res.send("Delete card");
   } catch (error) {
     next(error);
   }
--- routes/cardRouter.js
+++ routes/cardRouter.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { addCard } from "../controllers/cardControllers.js";
+import {
+  addCard,
+  updateCard,
+  deleteCard,
+} from "../controllers/cardControllers.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const cardRouter = express.Router();
 
 cardRouter.post("/:columnId/cards", authMiddleware, addCard);
-cardRouter.put("/:id", updateBook);
-cardRouter.delete("/", deleteCard);
+cardRouter.put("/cards/:cardId", updateCard);
+cardRouter.delete("/cards/:cardId", deleteCard);
 
 export default cardRouter;
