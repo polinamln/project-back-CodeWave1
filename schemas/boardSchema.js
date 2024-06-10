d5c648672b78805b2af8c49ed693979672ea21b9
naowmi <vadimpelyushenko@gmail.com>
2024-06-10 09:39:53 +0300

    fix

--- schemas/boardSchema.js
+++ schemas/boardSchema.js
@@ -4,5 +4,11 @@ export const boardSchema = Joi.object({
 title: Joi.string().required().messages({
     'string.empty': '"title" cannot be an empty field',
     'any.required': 'the required "title" field is missing',
-  })
+  }),
+icon: Joi.string().messages({
+  'string.empty': '"icon" cannot be an empty field'
+}),
+background: Joi.string().messages({
+  'string.empty': '"background" cannot be an empty field'
+})
 })
