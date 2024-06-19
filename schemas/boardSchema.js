6e3e16468a6aeeee3daa4819d5075903a4c10cb1
polinamln <polina77979.la@gmail.com>
2024-06-20 00:53:33 +0200

    Update boardSchema.js

--- schemas/boardSchema.js
+++ schemas/boardSchema.js
@@ -1,26 +1,26 @@
 import Joi from "joi";
 
 export const boardCreateSchema = Joi.object({
-title: Joi.string().required().messages({
-    'string.empty': '"title" cannot be an empty field',
-    'any.required': 'the required "title" field is missing',
+  title: Joi.string().required().messages({
+    "string.empty": '"title" cannot be an empty field',
+    "any.required": 'the required "title" field is missing',
   }),
-icon: Joi.string().messages({
-  'string.empty': '"icon" cannot be an empty field'
-}),
-background: Joi.string().messages({
-  'string.empty': '"background" cannot be an empty field'
-})
-})
+  icon: Joi.string().messages({
+    "string.empty": '"icon" cannot be an empty field',
+  }),
+  background: Joi.string().optional().messages({
+    "string.empty": '"background" cannot be an empty field',
+  }),
+});
 
 export const boardUpdateSchema = Joi.object({
   title: Joi.string().messages({
-      'string.empty': '"title" cannot be an empty field',
-    }),
+    "string.empty": '"title" cannot be an empty field',
+  }),
   icon: Joi.string().messages({
-    'string.empty': '"icon" cannot be an empty field'
+    "string.empty": '"icon" cannot be an empty field',
   }),
   background: Joi.string().messages({
-    'string.empty': '"background" cannot be an empty field'
-  })
-  })
+    "string.empty": '"background" cannot be an empty field',
+  }),
+});
